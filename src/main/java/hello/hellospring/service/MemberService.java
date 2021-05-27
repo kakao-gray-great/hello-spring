56026141cccc6cf8276b036ca71acc9963880435
oopsys6 <operasystem6@naver.com>
2021-05-27 17:48:02 +0900

    feat: 스프링 핵심 원리 예제 만들기 1

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,47 +1,11 @@
 package hello.hellospring.service;
 
-import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
-import java.util.List;
-import java.util.Optional;
+import hello.hellospring.member.Member;
 
-public class MemberService {
+public interface MemberService {
 
-    private final MemberRepository memberRepository;
+    void join(Member member);
 
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    /**
-     * 회원 가입
-     */
-    public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원 X
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
-    }
-
-    private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName())
-                .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
-    }
-
-    /**
-     * 전체 회원 조회
-     */
-    public List<Member> findMembers() {
-        return memberRepository.findAll();
-    }
-
-    public Optional<Member> findOne(Long memberId) {
-        return memberRepository.findById(memberId);
-    }
-
-
+    Member findMember(Long memberId);
 }
--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,69 +1,25 @@
 package hello.hellospring.service;
 
-import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.member.Grade;
+import hello.hellospring.member.Member;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemoryMemberRepository memberRepository;
-    MemberService memberService;
-
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
-
+    MemberService memberService = new MemberServiceImpl();
     @Test
-    void 회원가입() {
+    public void join() throws Exception {
+
         //given
-        Member member =  new Member();
-        member.setName("hello");
+        Member member = new Member(1L, "memberA", Grade.VIP);
 
         //when
-        Long saveId = memberService.join(member);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
 
         //then
-        Member findMember = memberService.findOne(saveId).get();
-        assertThat(member.getName()).isEqualTo(findMember.getName());
+        Assertions.assertThat(member).isEqualTo(findMember);
     }
 
-    @Test
-    void 중복_회원_예외() {
-        //given
-        Member member1 = new Member();
-        member1.setName("spring");
-
-        Member member2 = new Member();
-        member2.setName("spring");
-
-        //when
-        memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
-
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-
-        //then
-    }
-
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
-    }
 }
