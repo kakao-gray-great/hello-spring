97faf7a4734287cf7135898b8e6927d31cd3c4d8
oopsys6 <operasystem6@naver.com>
2021-06-15 11:13:06 +0900

    스프링 핵심 원리 (기본편) - 싱글톤 컨테이너

---
+++ src/test/java/hello/hellospring/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,33 @@
+package hello.hellospring.singleton;
+
+import hello.hellospring.AppConfig;
+import hello.hellospring.member.MemberRepository;
+import hello.hellospring.order.OrderServiceImpl;
+import hello.hellospring.service.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService --> memberRepository = " + memberRepository1);
+        System.out.println("orderService --> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
