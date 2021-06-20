bfff857cc848443e0a0cfe82e63a8b52f9e970b5
oopsys6 <operasystem6@naver.com>
2021-06-20 15:26:46 +0900

    스프링 핵심 원리 (기본편) - 빈 스코프 2

---
+++ src/test/java/hello/hellospring/scope/PrototypeProviderTest.java
@@ -0,0 +1,39 @@
+package hello.hellospring.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static hello.hellospring.scope.SingletonWithPrototypeTest1.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class PrototypeProviderTest {
+
+    @Test
+    void providerTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean1.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+
+    static class ClientBean {
+
+        @Autowired
+        private ApplicationContext ac;
+
+        public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+}
--- src/test/java/hello/hellospring/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/hellospring/scope/SingletonWithPrototypeTest1.java
@@ -1,7 +1,7 @@
 package hello.hellospring.scope;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -36,19 +36,17 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
